1ba87fa177d344e670925f7cfd7397c76ec96e6e
ira089 <gvir2015@gmail.com>
2024-11-11 22:57:06 +0200

    listUsers1

--- src/App.js
+++ src/App.js
@@ -1,14 +1,41 @@
-import React from 'react';
-import './index.scss';
-import { Success } from './components/Success';
-import { Users } from './components/Users';
+import React, { useState, useEffect } from "react";
+import "./index.scss";
+import { Success } from "./components/Success";
+import { Users } from "./components/Users";
 
 // Тут список пользователей: https://reqres.in/api/users
 
 function App() {
+  const [users, setUsers] = useState([]);
+  const [isLoading, setLoading] = useState(true);
+  const [searchValue, setSearchValue] = useState("");
+  const [invites, setInvites] = useState([]);
+
+  useEffect(() => {
+    fetch(`https://reqres.in/api/users`)
+      .then((res) => res.json())
+      .then((json) => {
+        setUsers(json.data);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert("error user");
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  const onChangeSearchValue = (evt) => {
+    setSearchValue(evt.target.value);
+  };
+
   return (
     <div className="App">
-      <Users />
+      <Users
+        items={users}
+        isLoading={isLoading}
+        searchValue={searchValue}
+        onChangeSearchValue={onChangeSearchValue}
+      />
       {/* <Success /> */}
     </div>
   );
