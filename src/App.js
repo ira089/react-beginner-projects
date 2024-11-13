8c5605e6b8bd89a5851c9420bb2fa049f6932c90
ira089 <gvir2015@gmail.com>
2024-11-13 20:45:07 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
-import React from 'react';
-import { Block } from './Block';
-import './index.scss';
+import React, { useEffect, useState } from "react";
+import { Block } from "./Block";
+import "./index.scss";
 
 function App() {
+  const [rates, setRates] = useState({});
+
+  useEffect(() => {
+    fetch();
+  }, []);
   return (
     <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
+      <Block
+        value={0}
+        currency="RUB"
+        onChangeCurrency={(cur) => console.log(cur)}
+      />
       <Block value={0} currency="USD" />
     </div>
   );
