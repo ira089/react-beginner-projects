62c35e1f22729218e1a13b4dfbe2979dc86fd6cf
ira089 <gvir2015@gmail.com>
2024-11-16 23:21:57 +0200

    photos2

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,71 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./index.scss";
 import Collection from "./Collection";
 
 function App() {
+  const cats = [
+    { name: "Все" },
+    { name: "Море" },
+    { name: "Горы" },
+    { name: "Архитектура" },
+    { name: "Города" },
+  ];
+  const [collections, setCollections] = useState([]);
+  const [searchValue, setSearchValue] = useState("");
+  const [categoryId, setCategoryId] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+
+  useEffect(() => {
+    setIsLoading(true);
+    fetch(
+      `https://6737b5d84eb22e24fca5ffb0.mockapi.io/api/v1/fotos?${
+        categoryId ? `category=${categoryId}` : ""
+      }`
+    )
+      .then((res) => res.json())
+      .then((json) => {
+        setCollections(json);
+      })
+      .catch((err) => {
+        console.warn(err);
+        alert(`error while receiving data`);
+      })
+      .finally(() => setIsLoading(false));
+  }, [categoryId]);
+
+  const handleChange = ({ target }) => {
+    setSearchValue(target.value.trim());
+  };
+
+  const elementCats = cats.map((obj, index) => (
+    <li
+      onClick={() => setCategoryId(index)}
+      className={categoryId === index ? "active" : ""}
+      key={obj.name}
+    >
+      {obj.name}
+    </li>
+  ));
+
+  const element = collections
+    .filter((obj) => obj.name.toLowerCase().includes(searchValue.toLowerCase()))
+    .map((obj, index) => (
+      <Collection key={index} name={obj.name} images={obj.photos} />
+    ));
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
-        <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
-        </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
-      </div>
-      <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-            "https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-            "https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-            "https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60",
-          ]}
+        <ul className="tags">{elementCats}</ul>
+        <input
+          value={searchValue}
+          onChange={handleChange}
+          className="search-input"
+          placeholder="Поиск по названию"
         />
       </div>
+      <div className="content">{isLoading ? <h2>Loading...</h2> : element}</div>
       <ul className="pagination">
         <li>1</li>
         <li className="active">2</li>
